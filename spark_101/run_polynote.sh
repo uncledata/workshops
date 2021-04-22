5694a33c01b16a03705ad5bb0b3ff59a6db61751
tomaspe <tomasp@wix.com>
2021-04-22 09:15:38 +0300

    polynote addition with scripts

---
+++ spark_101/run_polynote.sh
@@ -0,0 +1,2 @@
+docker build -it polynote_local:latest .
+docker run --rm -it -p 127.0.0.1:8192:8192 -p 127.0.0.1:4040-4050:4040-4050 -v `pwd`/config.yml:/opt/config/config.yml -v `pwd`/notebooks:/opt/notebooks/ polynote_local:latest --config /opt/config/config.yml
