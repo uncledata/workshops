ed0dd5ff6de8e347e1e957249218e1797840af57
Tomas Peluritis <tomas1991@gmail.com>
2022-11-04 20:33:01 +0200

    small tweaks once more

--- dev_env_101/airflow-local-docker/scripts/scheduler_entrypoint.sh
+++ dev_env_101/airflow-local-docker/scripts/scheduler_entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-chown 50000:0 dags logs plugins
+chown 50000:0 ${AIRFLOW_HOME}/dags ${AIRFLOW_HOME}/logs ${AIRFLOW_HOME}/plugins
 
-cd /opt/airflow/transform/data_warehouse && dbt clean && dbt deps
+cd ${AIRFLOW_HOME}/transform/data_warehouse && dbt clean && dbt deps
 psql postgresql://airflow:airflow@192.168.1.3:5432/postgres -c "SELECT 1 FROM pg_database WHERE datname = 'data_warehouse'" | grep -q 1 || psql postgresql://airflow:airflow@192.168.1.3:5432/postgres -c "CREATE DATABASE data_warehouse"
-cd /opt/airflow
+cd $AIRFLOW_HOME
 airflow db init;
 exec airflow scheduler
 
