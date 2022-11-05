ff73481cf4290098ffbac878112d3079c11862b8
Tomas Peluritis <tomas1991@gmail.com>
2022-11-05 15:29:51 +0200

    drop dev env

--- dev_env_101/airflow-local-docker/scripts/scheduler_entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /opt/airflow/transform/data_warehouse && dbt clean && dbt deps
-psql postgresql://airflow:airflow@192.168.1.3:5432/postgres -c "SELECT 1 FROM pg_database WHERE datname = 'data_warehouse'" | grep -q 1 || psql postgresql://airflow:airflow@192.168.1.3:5432/postgres -c "CREATE DATABASE data_warehouse"
-cd /opt/airflow
-airflow db init;
-exec airflow scheduler
-
--- dev_env_101/airflow-local-docker/scripts/webserver_entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-airflow users create -u admin -p admin -r Admin -e test@test.com -f admin -l admin ;
-airflow webserver
